f8271dc5c8bb55e7bba553c9814dd385b188a5f1
wuhang100 <36547153+wuhang100@users.noreply.github.com>
2019-06-11 22:52:04 +0800

    server

--- test2/src/server.cpp
+++ test2/src/server.cpp
@@ -1,10 +1,30 @@
 #include <ros/ros.h>
 #include <test2/myservice.h>
 #include <string>
+#include <vector>  
+#include <string> 
+#include <fstream> 
+#include <sstream>
+#include <cstring>
 
 bool handle_fun(test2::myservice::Request &req, test2::myservice::Response &res)
 {
+    std::ifstream myfile("/home/wuhang/projects/example.txt");
     res.sum = req.a+req.b;
+    std::vector<std::string> temp; 
+    std::string myline;
+    std::int32_t c;
+    if (!myfile.is_open()) 
+    { 
+        ROS_INFO("open fail");
+    }     
+    while(getline(myfile,myline)) 
+    { 
+        temp.push_back(myline);
+    }
+    myfile.close(); 
+    c = stoi(temp[0]);
+    res.sum = req.a+req.b+c;
     res.feedback = "My answer is " + std::to_string(res.sum);
     return true;
 }
